bbdccc5b25258788c01f19f5328e47f4b201ac10
Eduardo Santos Nascimento <eduardosantosnascimento@esn127d1001314.sp.local>
2023-01-05 13:09:49 -0300

    aula : 01

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main (){
+    cout << "***************************************" << endl;
+    cout << "* Boas vindas ao Jogo da Adivinhação! *" << endl;
+    cout << "***************************************" << endl;
+
+    int secretNumber = 32;
+    
+    cout << " *** " << secretNumber << " ***" << endl;
+
+}
+
